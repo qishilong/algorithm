4b54b754a53898327d3ef85f19f0dc252d8f9a21
qishilong <663253062@qq.com>
2023-10-24 21:08:38 +0800

    更新转置矩阵
    更新转置矩阵

--- LeetCode/867. 转置矩阵/867. 转置矩阵.ts
+++ LeetCode/867. 转置矩阵/867. 转置矩阵.ts
@@ -1,20 +1,33 @@
+const matrix = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9],
+];
+const matrix1 = [
+	[1, 2, 3],
+	[4, 5, 6],
+];
+
 // 解法一
 function transpose(matrix: number[][]): number[][] {
-    const itemLength = matrix[0].length;
-    const length = matrix.length;
-    const resultArr: number[][] = [];
-    for (let i = 0; i < length; i++) {
-        for (let j = 0; j < itemLength; j++) {
-            if (!resultArr[j]) {
-                resultArr[j] = [];
-            }
-            resultArr[j][i] = matrix[i][j];
-        }
-    }
-    return resultArr;
-};
+	const itemLength = matrix[0].length;
+	const length = matrix.length;
+	const resultArr: number[][] = [];
+	for (let i = 0; i < length; i++) {
+		for (let j = 0; j < itemLength; j++) {
+			if (!resultArr[j]) {
+				resultArr[j] = [];
+			}
+			resultArr[j][i] = matrix[i][j];
+		}
+	}
+	return resultArr;
+}
 
 // 解法二
 // function transpose(matrix: number[][]): number[][] {
 //     return matrix[0].map((item, i) => matrix.map((v, j) => v[i]));
 // };
+
+const result = transpose(matrix)
+console.log(result)
