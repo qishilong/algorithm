4406640b65869fafd01ece31c1ba5af8f6d24775
qishilong <663253062@qq.com>
2023-11-18 19:04:00 +0800

    feat(leetcode): 19. 删除链表的倒数第 N 个结点
    feat(leetcode): 19. 删除链表的倒数第 N 个结点

---
+++ LeetCode/19. 删除链表的倒数第 N 个结点/19. 删除链表的倒数第 N 个结点.js
@@ -0,0 +1,20 @@
+const {ListNode} = require("./../../notes/链表/单链表链表的构造函数.js");
+
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+  const dum = new ListNode(0, head);
+  let slow = (fast = dum);
+  while (n--) {
+    fast = fast.next;
+  }
+  while (fast && fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  slow.next = slow.next.next;
+  return dum.next;
+};
