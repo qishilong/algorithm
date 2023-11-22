4e9b14fcd3e50425975ac74d63098397faec051a
qishilong <663253062@qq.com>
2023-11-22 11:04:11 +0800

    feat(leetcode): 76. 最小覆盖子串
    feat(leetcode): 76. 最小覆盖子串

---
+++ LeetCode/76. 最小覆盖子串/76. 最小覆盖子串.js
@@ -0,0 +1,58 @@
+/**
+ * 滑动窗口
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function (s, t) {
+  const need = new Map(),
+    window = new Map();
+  for (let i = 0, len = t.length; i < len; i++) {
+    if (need.has(t[i])) {
+      need.set(t[i], need.get(t[i]) + 1);
+    } else {
+      need.set(t[i], 1);
+    }
+  }
+  console.log(need);
+  let left = 0,
+    right = 0,
+    valid = 0;
+  let start = 0,
+    len = Infinity;
+  while (right < s.length) {
+    const c = s[right];
+    right++;
+    if (need.has(c)) {
+      if (window.has(c)) {
+        window.set(c, window.get(c) + 1);
+      } else {
+        window.set(c, 1);
+      }
+      if (window.get(c) === need.get(c)) {
+        valid++;
+      }
+    }
+    while (valid === need.size) {
+      if (right - left < len) {
+        start = left;
+        len = right - left;
+      }
+      const d = s[left];
+      left++;
+      if (need.has(d)) {
+        if (window.get(d) === need.get(d)) {
+          valid--;
+        }
+        window.set(d, window.get(d) - 1);
+      }
+    }
+  }
+  return len === Infinity ? "" : s.slice(start, start + len);
+};
+
+const s = "abc",
+  t = "b";
+
+const result = minWindow(s, t);
+console.log(result);
