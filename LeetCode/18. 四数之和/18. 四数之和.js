ec81d381eb3b23b48c6713789c9baf9c7104889f
qishilong <663253062@qq.com>
2023-11-27 15:45:16 +0800

    feat(leetcode): 18. 四数之和
    feat(leetcode): 18. 四数之和

---
+++ LeetCode/18. 四数之和/18. 四数之和.js
@@ -0,0 +1,55 @@
+const { quickSortFn } = require("../../notes/tools/快速排序.js");
+
+/**
+ * 双指针
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function (nums, target) {
+  if (!nums || nums.length < 4) {
+    return [];
+  }
+  quickSortFn(nums);
+  const result = [],
+    len = nums.length;
+  for (let i = 0; i < len - 3; i++) {
+    // 去重
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+    for (let j = i + 1; j < len - 2; j++) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) {
+        continue;
+      }
+      let left = j + 1,
+        right = len - 1;
+      while (left < right) {
+        const sum = nums[i] + nums[j] + nums[left] + nums[right];
+        if (sum > target) {
+          right--;
+          continue;
+        } else if (sum < target) {
+          left++;
+          continue;
+        } else {
+          result.push([nums[i], nums[j], nums[right], nums[left]]);
+          // 对 nums[left] 和 nums[right] 进行去重
+          while (left < right && nums[left] === nums[left + 1]) {
+            left++;
+          }
+          while (left < right && nums[right] === nums[right - 1]) {
+            right--;
+          }
+          left++;
+          right--;
+        }
+      }
+    }
+  }
+  return result;
+};
+const nums = [2, 2, 2, 2, 2],
+  target = 8;
+const result = fourSum(nums, target);
+console.log(result);
