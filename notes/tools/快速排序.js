95f0dd703a6eedc13bbb103c078bff250d3cf625
qishilong <663253062@qq.com>
2023-11-27 15:44:55 +0800

    feat(notes): tools - 快速排序
    feat(notes): tools - 快速排序

---
+++ notes/tools/快速排序.js
@@ -0,0 +1,44 @@
+// const arr = [1, 4, 1, 3, 1512, 451234, 15, 1, 412, 312, 51, 251, 341];
+
+const swap = (arr, left, right) => {
+  const temp = arr[left];
+  arr[left] = arr[right];
+  arr[right] = temp;
+};
+
+const quicksort = (arr, begin, end) => {
+  if (begin >= end - 1) {
+    return arr;
+  }
+  let left = begin,
+    right = end;
+  do {
+    do {
+      left++;
+    } while (left < right && arr[left] < arr[begin]);
+    do {
+      right--;
+    } while (left < right && arr[right] > arr[begin]);
+    if (left < right) {
+      swap(arr, left, right);
+    }
+  } while (left < right);
+  const pointIndex = left === right ? right - 1 : right;
+  swap(arr, begin, pointIndex);
+  quicksort(arr, begin, pointIndex);
+  quicksort(arr, pointIndex + 1, end);
+};
+
+const quickSortFn = (arr) => {
+  if (!arr || arr.length < 2) {
+    return arr;
+  }
+  quicksort(arr, 0, arr.length);
+};
+// quickSortFn(arr);
+
+// console.log(arr);
+
+module.exports = {
+  quickSortFn
+};
