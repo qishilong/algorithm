a46dc745f028b8376d976e42d38e4338903d3aac
qishilong <663253062@qq.com>
2023-12-04 16:48:28 +0800

    ci(leetcode): 541. 反转字符串 II
    ci(leetcode): 541. 反转字符串 II

---
+++ LeetCode/541. 反转字符串 II/541. 反转字符串 II.js
@@ -0,0 +1,24 @@
+const { swap } = require("./../../notes/tools/交换.js");
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function (s, k) {
+  const strArr = s.split("");
+
+  // 每隔 2k 个字符的前 k 个字符进行翻转
+  for (let i = 0, len = strArr.length; i < len; i += 2 * k) {
+    let left = i - 1,
+      right = i + k > len ? len : i + k;
+    while (++left < --right) {
+      swap(strArr, left, right);
+    }
+  }
+  return strArr.join("");
+};
+const s = "abcdefg",
+  k = 2;
+const result = reverseStr(s, k);
+console.log(result);
