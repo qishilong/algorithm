5da5892a4718e48b9d585d4619cebe224980571a
qishilong <663253062@qq.com>
2023-12-04 11:10:42 +0800

    ci(leetcode): 344. 反转字符串
    ci(leetcode): 344. 反转字符串

---
+++ LeetCode/344. 反转字符串/344. 反转字符串.js
@@ -0,0 +1,16 @@
+const { swap } = require("./../../notes/tools/交换.js");
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+    swap(s, i, j);
+  }
+  return s;
+};
+
+const s = ["h", "e", "l", "l", "o"];
+const result = reverseString(s);
+console.log(result);
