152a9e3840c7cadc669d8756f9b2dbf0d01f3365
qishilong <663253062@qq.com>
2023-12-13 20:26:58 +0800

    ci(LeetCode): 347. 前 K 个高频元素
    增加利用堆的解法
    新增堆的构造类

--- LeetCode/347. 前 K 个高频元素/347. 前 K 个高频元素.js
+++ LeetCode/347. 前 K 个高频元素/347. 前 K 个高频元素.js
@@ -1,28 +1,64 @@
+const { Heap } = require("./../../notes/tools/二叉堆的构建类.js");
+
 /**
  * 1. Map
  * @param {number[]} nums
  * @param {number} k
  * @return {number[]}
  */
+// var topKFrequent = function (nums, k) {
+//   if (nums.length === k) {
+//     return nums;
+//   }
+//   const map = new Map();
+//   for (let i = 0, len = nums.length; i < len; i++) {
+//     if (map.has(nums[i])) {
+//       map.set(nums[i], map.get(nums[i]) + 1);
+//     } else {
+//       map.set(nums[i], 1);
+//     }
+//   }
+//   const arr = [];
+//   map.forEach((val, key) => arr.push([val, key]));
+//   arr.sort((a, b) => b[0] - a[0]);
+//   const result = [];
+//   for (let i = 0; i < k; i++) {
+//     result.push(arr[i][1]);
+//   }
+//   return result;
+// };
+
+/**
+ * 2. 利用堆
+ * @param {*} nums
+ * @param {*} k
+ */
 var topKFrequent = function (nums, k) {
-  if (nums.length === k) {
-    return nums;
-  }
   const map = new Map();
-  for (let i = 0, len = nums.length; i < len; i++) {
-    if (map.has(nums[i])) {
-      map.set(nums[i], map.get(nums[i]) + 1);
-    } else {
-      map.set(nums[i], 1);
+  for (const num of nums) {
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+
+  // 创建小顶堆
+  const heap = new Heap((a, b) => a[1] - b[1]);
+
+  // entry 是一个长度为 2 的数组，0 位置存储 key，1 位置存储 value
+  const entries = map.entries();
+
+  for (const entry of entries) {
+    heap.push(entry);
+
+    if (heap.size() > k) {
+      heap.pop();
     }
   }
-  const arr = [];
-  map.forEach((val, key) => arr.push([val, key]));
-  arr.sort((a, b) => b[0] - a[0]);
+
   const result = [];
-  for (let i = 0; i < k; i++) {
-    result.push(arr[i][1]);
+
+  for (let i = heap.size() - 1; i >= 0; i--) {
+    result[i] = heap.pop()[0];
   }
+
   return result;
 };
 
---
+++ notes/tools/二叉堆的构建类.js
@@ -0,0 +1,76 @@
+const { swap } = require("./../tools/交换");
+
+/**
+ * 二叉堆的构建类
+ */
+class Heap {
+  constructor(compareFn) {
+    this.compareFn = compareFn;
+    this.queue = [];
+  }
+
+  // 推入元素
+  push(val) {
+    // 推入
+    this.queue.push(val);
+
+    // 上浮
+    let index = this.size() - 1; // 记录推入元素下标
+    let parent = Math.floor((index - 1) / 2); // 记录父节点下标
+
+    // 注意 compare 参数的顺序
+    while (parent >= 0 && this.compare(parent, index) > 0) {
+      // 交换当前节点和父节点的位置
+      swap(this.queue, index, parent);
+
+      index = parent;
+      parent = Math.floor((index - 1) / 2);
+    }
+  }
+  // 获取堆顶元素并移除
+  pop() {
+    // 堆顶元素
+    const out = this.queue[0];
+
+    // 移除堆顶元素，填入最后一个元素
+    this.queue[0] = this.queue.pop();
+
+    // 下沉
+    let index = 0; // 记录下沉元素下标
+    let left = 1; // left 是左子节点下标 left + 1 是右子节点下标
+    let searchChild = this.compare(left, left + 1) > 0 ? left + 1 : left;
+
+    // 注意 compare 参数顺序
+    while (searchChild !== undefined && this.compare(index, searchChild) > 0) {
+      swap(this.queue, index, searchChild);
+
+      // 更新下标
+      index = searchChild;
+      left = index * 2 + 1;
+      searchChild = this.compare(left, left + 1) > 0 ? left + 1 : left;
+    }
+
+    return out;
+  }
+
+  // 返回堆内元素的数量
+  size() {
+    return this.queue.length;
+  }
+
+  // 使用传入的 compareFn 比较两个位置的元素
+  compare(index1, index2) {
+    // 处理下标越界问题
+    if (this.queue[index1] === undefined) {
+      return 1;
+    }
+    if (this.queue[index2] === undefined) {
+      return -1;
+    }
+    return this.compareFn(this.queue[index1], this.queue[index2]);
+  }
+}
+
+module.exports = {
+  Heap,
+};
