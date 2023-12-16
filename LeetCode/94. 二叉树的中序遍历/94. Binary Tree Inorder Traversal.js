1235400eebe78b7025aa528df5e9ee4f00dbda8e
qishilong <663253062@qq.com>
2023-12-16 22:26:33 +0800

    ci(leetcode): 94. 二叉树的中序遍历
    ci(leetcode): 94. 二叉树的中序遍历

---
+++ LeetCode/94. 二叉树的中序遍历/94. Binary Tree Inorder Traversal.js
@@ -0,0 +1,37 @@
+const { TreeNode } = require("./../../notes/二叉树/二叉树的构造方法.js");
+
+const root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root, result = []) {
+  if (!root) {
+    return result;
+  }
+
+  const dfs = (root) => {
+    if (!root) {
+      return result;
+    }
+    dfs(root.left);
+    result.push(root.val);
+    dfs(root.right);
+  };
+
+  dfs(root);
+
+  return result;
+};
+
+const result = preorderTraversal(root);
+console.log(result);
