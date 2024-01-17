1294cc8c7609aea87c13c4f3cd2ddd4ed09893fb
qishilong <663253062@qq.com>
2024-01-17 23:52:53 +0800

    ci(leetcode): 104. 二叉树的最大深度

--- LeetCode/104. 二叉树的最大深度/104. 二叉树的最大深度.js
+++ LeetCode/104. 二叉树的最大深度/104. 二叉树的最大深度.js
@@ -1,39 +1,3 @@
-# 104. 二叉树的最大深度
-
-给定一个二叉树 `root` ，返回其最大深度。
-
-二叉树的 **最大深度** 是指从根节点到最远叶子节点的最长路径上的节点数。
-
- 
-
-**示例 1：**
-
-![img](https://qiniucloud.qishilong.space/images/202308222237820.jpg)
-
- 
-
-```
-输入：root = [3,9,20,null,null,15,7]
-输出：3
-```
-
-**示例 2：**
-
-```
-输入：root = [1,null,2]
-输出：2
-```
-
- 
-
-**提示：**
-
--   树中节点的数量在 `[0, 104]` 区间内。
--   `-100 <= Node.val <= 100`
-
-## 代码
-
-```ts
 /**
  * 1. 递归（后序遍历）
  * @param {TreeNode} root
@@ -124,10 +88,3 @@ var maxDepth = function (root) {
   }
   return result;
 };
-```
-
-### 思路
-
->   分治思想
->
->   ![image-20230822223804249](https://qiniucloud.qishilong.space/images/202308222238279.png)
