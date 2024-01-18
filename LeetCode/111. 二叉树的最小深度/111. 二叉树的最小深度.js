b358fad3a57b5fefb36bcf5eee41756e75a0af05
qishilong <663253062@qq.com>
2024-01-18 21:17:26 +0800

    ci(leetcode): 111. 二叉树的最小深度

--- LeetCode/111. 二叉树的最小深度/111. 二叉树的最小深度.js
+++ LeetCode/111. 二叉树的最小深度/111. 二叉树的最小深度.js
@@ -1,39 +1,3 @@
-# [111. 二叉树的最小深度](https://leetcode.cn/problems/minimum-depth-of-binary-tree/)
-
-给定一个二叉树，找出其最小深度。
-
-最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
-
-**说明：**叶子节点是指没有子节点的节点。
-
- 
-
-**示例 1：**
-
-![img](https://qiniucloud.qishilong.space/images/202308222241187.jpg)
-
-```
-输入：root = [3,9,20,null,null,15,7]
-输出：2
-```
-
-**示例 2：**
-
-```
-输入：root = [2,null,3,null,4,null,5,null,6]
-输出：5
-```
-
- 
-
-**提示：**
-
--   树中节点数的范围在 `[0, 105]` 内
--   `-1000 <= Node.val <= 1000`
-
-## 代码
-
-```js
 /**
  * 1. 递归（后序遍历）
  * @param {TreeNode} root
@@ -128,32 +92,3 @@ var minDepth = function (root) {
   }
   return depth;
 };
-```
-
-```ts
-function minDepth(root: TreeNode | null): number {
-  if (root === null) {
-    return 0;
-  }
-  if (!root.left || !root.right) {
-    return Math.max(minDepth(root.left), minDepth(root.right)) + 1;
-  }
-  return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
-}
-```
-
-### 思路
-
->   ![image-20230822224207847](https://qiniucloud.qishilong.space/images/202308222242881.png)
-
-### 细节
-
-在计算 `root = [2,null,3,null,4,null,5,null,6]` 类型树时，需要加一层判断
-
-```ts
-if(!root.left || !root.right){
-    return Math.max(minDepth(root.left), minDepth(root.right)) + 1;
-}
-```
-
-这时只有最大深度，没有最小深度
