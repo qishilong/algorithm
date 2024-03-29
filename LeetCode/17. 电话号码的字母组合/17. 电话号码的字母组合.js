4a7f890a337d45d63b6c61b76d4c2b97889081d1
qishilong <663253062@qq.com>
2024-03-29 15:17:22 +0800

    ci(leetcode): 17. 电话号码的字母组合

--- LeetCode/17. 电话号码的字母组合/17. 电话号码的字母组合.js
+++ LeetCode/17. 电话号码的字母组合/17. 电话号码的字母组合.js
@@ -3,51 +3,51 @@
  * @param {string} digits
  * @return {string[]}
  */
-var letterCombinations = function (digits) {
-  const length = digits.length;
+// var letterCombinations = function (digits) {
+//   const length = digits.length;
 
-  if (length === 0) {
-    return [];
-  }
+//   if (length === 0) {
+//     return [];
+//   }
 
-  const letterArr = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+//   const letterArr = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
 
-  const result = [], // 存放结果
-    path = []; // 可能项
+//   const result = [], // 存放结果
+//     path = []; // 可能项
 
-  const backtracking = (digits, index) => {
-    if (index === length) {
-      result.push(path.join(""));
-      return;
-    }
+//   const backtracking = (digits, index) => {
+//     if (index === length) {
+//       result.push(path.join(""));
+//       return;
+//     }
 
-    if (digits[index] === "1") {
-      // 处理输入包括'1'的情况
-      result.push(" ");
-      return;
-    } else if (digits[index] === "*") {
-      // 处理输入包括'*'的情况
-      result.push("*");
-      return;
-    } else if (digits[index] === "#") {
-      // 处理输入包括'#'的情况
-      result.push("#");
-      return;
-    } else {
-      const letter = letterArr[digits[index]]; // 取数字对应的字符集
+//     if (digits[index] === "1") {
+//       // 处理输入包括'1'的情况
+//       result.push(" ");
+//       return;
+//     } else if (digits[index] === "*") {
+//       // 处理输入包括'*'的情况
+//       result.push("*");
+//       return;
+//     } else if (digits[index] === "#") {
+//       // 处理输入包括'#'的情况
+//       result.push("#");
+//       return;
+//     } else {
+//       const letter = letterArr[digits[index]]; // 取数字对应的字符集
 
-      for (let i = 0; i < letter.length; i++) {
-        path.push(letter[i]); // 处理
-        backtracking(digits, index + 1); // 递归，注意 index + 1，下一层要处理下一个数字
-        path.pop(); // 回溯
-      }
-    }
-  };
+//       for (let i = 0; i < letter.length; i++) {
+//         path.push(letter[i]); // 处理
+//         backtracking(digits, index + 1); // 递归，注意 index + 1，下一层要处理下一个数字
+//         path.pop(); // 回溯
+//       }
+//     }
+//   };
 
-  backtracking(digits, 0);
+//   backtracking(digits, 0);
 
-  return result;
-};
+//   return result;
+// };
 
 /**
  * 2. 把回溯过程放递归函数里
