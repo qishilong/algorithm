74737f9d556367d041ddcb6f75ea60c5b7eb578e
qishilong <663253062@qq.com>
2024-04-30 14:45:06 +0800

    ci(leetcode): 46. 全排列

--- LeetCode/46. 全排列/46. 全排列.js
+++ LeetCode/46. 全排列/46. 全排列.js
@@ -2,4 +2,40 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function (nums) {};
+var permute = function (nums) {
+  if (!nums || nums.length === 0) {
+    return [[]];
+  }
+  if (nums.length === 1) {
+    return [nums];
+  }
+
+  const result = [],
+    path = [],
+    length = nums.length,
+    used = new Array(length).fill(false);
+
+  const backtracking = (nums, path, used) => {
+    // 说明找到了一组
+    if (path.length === length) {
+      result.push([...path]);
+      return;
+    }
+
+    for (let i = 0; i < length; i++) {
+      // 说明当前的元素已经被使用过，直接跳过
+      if (used[i]) {
+        continue;
+      }
+      used[i] = true;
+      path.push(nums[i]);
+      backtracking(nums, path, used);
+      path.pop(); // 回溯
+      used[i] = false; // 回溯
+    }
+  };
+
+  backtracking(nums, path, used);
+
+  return result;
+};
