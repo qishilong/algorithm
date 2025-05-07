5258caed5ecfee70469dc785dc37c59f8373bd92
qishilong <663253062@qq.com>
2025-05-07 21:00:02 +0800

    chore(面试经典 150 题): 80. 删除有序数组中的重复项 II

--- 面试经典 150 题/80. 删除有序数组中的重复项 II.js
+++ 面试经典 150 题/80. 删除有序数组中的重复项 II.js
@@ -3,30 +3,30 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function (nums) {
-  if (!nums || !Array.isArray(nums) || nums.length === 0) {
-    return 0
-  }
+// var removeDuplicates = function (nums) {
+//   if (!nums || !Array.isArray(nums) || nums.length === 0) {
+//     return 0
+//   }
 
-  if (nums.length < 3) {
-    return nums.length
-  }
+//   if (nums.length < 3) {
+//     return nums.length
+//   }
 
-  let count = 1
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] === nums[i - 1]) {
-      count++
-    } else {
-      count = 1
-    }
-    if (count > 2) {
-      nums.splice(i, 1)
-      i--
-    }
-  }
+//   let count = 1
+//   for (let i = 1; i < nums.length; i++) {
+//     if (nums[i] === nums[i - 1]) {
+//       count++
+//     } else {
+//       count = 1
+//     }
+//     if (count > 2) {
+//       nums.splice(i, 1)
+//       i--
+//     }
+//   }
 
-  return nums.length
-}
+//   return nums.length
+// }
 
 /**
  * 2. 快慢指针
