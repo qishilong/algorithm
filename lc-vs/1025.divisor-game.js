ef3bcf9f2b632cc255d5d678ef73486e9eb2b3a7
qishilong <663253062@qq.com>
2025-11-10 11:27:45 +0800

    ci(lc-vs): 1025.除数博弈

---
+++ lc-vs/1025.divisor-game.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=1025 lang=javascript
+ * @lcpr version=30204
+ *
+ * [1025] 除数博弈
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var divisorGame = function (n) {
+  return n % 2 === 0;
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// 2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 3\n
+// @lcpr case=end
+
+ */
