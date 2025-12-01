0260421f4be7cdd2ff01987369311f4fec28c06e
qishilong <663253062@qq.com>
2025-12-01 17:26:07 +0800

    ci(lc-vs): 58. 最后一个单词的长度

---
+++ lc-vs/58.length-of-last-word.js
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=58 lang=javascript
+ * @lcpr version=30204
+ *
+ * [58] 最后一个单词的长度
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  if (!s || typeof s !== "string" || !s.trim()) {
+    return;
+  }
+
+  const arr = s.trim().split(" ");
+
+  return arr[arr.length - 1].length;
+};
+
+const s = "Hello World";
+const res = lengthOfLastWord(s);
+console.log(res);
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// "Hello World"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "   fly me   to   the moon  "\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "luffy is still joyboy"\n
+// @lcpr case=end
+
+ */
