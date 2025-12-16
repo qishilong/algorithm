e9f88d3a09690ae275b45c28f5f2f1b4f0d1d6b8
qishilong <663253062@qq.com>
2025-12-16 15:07:35 +0800

    ci(lc-vs): 4. 寻找两个正序数组的中位数

--- lc-vs/4.median-of-two-sorted-arrays.js
+++ lc-vs/4.median-of-two-sorted-arrays.js
@@ -14,27 +14,96 @@
  * @param {number[]} nums2
  * @return {number}
  */
+// var findMedianSortedArrays = function (nums1, nums2) {
+//   if (
+//     !nums1 ||
+//     !nums2 ||
+//     !Array.isArray(nums1) ||
+//     !Array.isArray(nums2) ||
+//     (nums1.length === 0 && nums2.length === 0)
+//   ) {
+//     return;
+//   }
+
+//   const getMid = (arr) => {
+//     if (arr.length === 0) {
+//       return;
+//     }
+//     if (arr.length === 1) {
+//       return arr[0];
+//     }
+
+//     const length = arr.length;
+//     const midIndex = Math.floor(length / 2);
+//     if (length % 2 === 1) {
+//       return arr[midIndex];
+//     } else {
+//       return (arr[midIndex] + arr[midIndex - 1]) / 2;
+//     }
+//   };
+
+//   let length1 = nums1.length,
+//     length2 = nums2.length;
+
+//   const arr = new Array(length1 + length2).fill(0);
+
+//   while (length1 >= 0 || length2 >= 0) {
+//     if (length1 === 0 && length2 > 0) {
+//       const arrLast = length2 - 1;
+//       arr[arrLast] = nums2[arrLast];
+//       length2--;
+//     } else if (length1 > 0 && length2 === 0) {
+//       const arrLast = length1 - 1;
+//       arr[arrLast] = nums1[arrLast];
+//       length1--;
+//     } else if (length1 > 0 && length2 > 0) {
+//       const last1 = length1 - 1,
+//         last2 = length2 - 1,
+//         arrLast = last1 + last2 + 1;
+//       if (nums1[last1] > nums2[last2]) {
+//         arr[arrLast] = nums1[last1];
+//         length1--;
+//       } else if (nums1[last1] === nums2[last2]) {
+//         length1--;
+//         length2--;
+//         arr[arrLast] = nums1[last1];
+//         arr[arrLast - 1] = nums2[last2];
+//       } else {
+//         arr[arrLast] = nums2[last2];
+//         length2--;
+//       }
+//     } else {
+//       break;
+//     }
+//   }
+
+//   const res = getMid(arr);
+
+//   return res;
+// };
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
 var findMedianSortedArrays = function (nums1, nums2) {
-  if (
-    !nums1 ||
-    !nums2 ||
-    !Array.isArray(nums1) ||
-    !Array.isArray(nums2) ||
-    (nums1.length === 0 && nums2.length === 0)
-  ) {
+  if (!Array.isArray(nums1) && !Array.isArray(nums2)) {
     return;
   }
 
+  /**
+   * 求中位数
+   * @param {number[]} arr
+   */
   const getMid = (arr) => {
     if (arr.length === 0) {
       return;
     }
-    if (arr.length === 1) {
-      return arr[0];
-    }
 
     const length = arr.length;
     const midIndex = Math.floor(length / 2);
+
     if (length % 2 === 1) {
       return arr[midIndex];
     } else {
@@ -42,44 +111,40 @@ var findMedianSortedArrays = function (nums1, nums2) {
     }
   };
 
+  if (Array.isArray(nums1) && !Array.isArray(nums2)) {
+    return getMid(nums1);
+  }
+
+  if (!Array.isArray(nums1) && Array.isArray(nums2)) {
+    return getMid(nums2);
+  }
+
   let length1 = nums1.length,
     length2 = nums2.length;
 
-  const arr = new Array(length1 + length2).fill(0);
+  const res = new Array(length1 + length2).fill(0);
 
-  while (length1 >= 0 || length2 >= 0) {
-    if (length1 === 0 && length2 > 0) {
-      const arrLast = length2 - 1;
-      arr[arrLast] = nums2[arrLast];
-      length2--;
-    } else if (length1 > 0 && length2 === 0) {
-      const arrLast = length1 - 1;
-      arr[arrLast] = nums1[arrLast];
-      length1--;
-    } else if (length1 > 0 && length2 > 0) {
-      const last1 = length1 - 1,
-        last2 = length2 - 1,
-        arrLast = last1 + last2 + 1;
-      if (nums1[last1] > nums2[last2]) {
-        arr[arrLast] = nums1[last1];
-        length1--;
-      } else if (nums1[last1] === nums2[last2]) {
-        length1--;
-        length2--;
-        arr[arrLast] = nums1[last1];
-        arr[arrLast - 1] = nums2[last2];
-      } else {
-        arr[arrLast] = nums2[last2];
-        length2--;
-      }
+  while (length1 > 0 || length2 > 0) {
+    const val1 = nums1[length1 - 1] !== undefined ? nums1[length1 - 1] : -Infinity;
+    const val2 = nums2[length2 - 1] !== undefined ? nums2[length2 - 1] : -Infinity;
+
+    const resLastIndex = length1 + length2 - 1;
+
+    if (val1 > val2) {
+      res[resLastIndex] = val1;
+      --length1;
+    } else if (val1 === val2) {
+      res[resLastIndex] = val1;
+      --length1;
+      res[resLastIndex - 1] = val2;
+      --length2;
     } else {
-      break;
+      res[resLastIndex] = val2;
+      --length2;
     }
   }
 
-  const res = getMid(arr);
-
-  return res;
+  return getMid(res);
 };
 
 const nums1 = [1, 3],
