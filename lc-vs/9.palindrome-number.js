13a08531e443c4becea73922f2cbeb80d50b640a
qishilong <663253062@qq.com>
2025-12-17 14:50:18 +0800

    ci(lc-vs): 9. 回文数

--- lc-vs/9.palindrome-number.js
+++ lc-vs/9.palindrome-number.js
@@ -46,38 +46,104 @@
  * @param {number} x
  * @return {boolean}
  */
+// var isPalindrome = function (x) {
+//   if ((x !== 0 && !x) || x < 0) {
+//     return false;
+//   }
+
+//   if (x === 0) {
+//     return true;
+//   }
+
+//   let number = Math.abs(x);
+
+//   const xArr = [];
+
+//   while (number) {
+//     xArr.unshift(number % 10);
+//     number = Math.floor(number / 10);
+//   }
+
+//   let left = 0,
+//     right = xArr.length - 1;
+
+//   let result = true;
+
+//   while (left <= right) {
+//     if (xArr[left] !== xArr[right]) {
+//       return false;
+//     }
+//     left++;
+//     right--;
+//   }
+
+//   return result;
+// };
+
+/**
+ * 3. 数字转换为字符串
+ * @param {number} x
+ * @return {boolean}
+ */
+// var isPalindrome = function (x) {
+//   if (typeof x !== "number") {
+//     return;
+//   }
+
+//   if (x < 0) {
+//     return false;
+//   }
+
+//   const xStr = String(x);
+
+//   let left = 0,
+//     right = xStr.length - 1;
+
+//   while (left < right) {
+//     if (xStr[left] !== xStr[right]) {
+//       return false;
+//     }
+//     ++left;
+//     --right;
+//   }
+
+//   return true;
+// };
+
+/**
+ * 4. 获取每一位数字
+ * 利用数组获取数字中的每一位数字
+ * @param {number} x
+ * @return {boolean}
+ */
 var isPalindrome = function (x) {
-  if ((x !== 0 && !x) || x < 0) {
+  if (typeof x !== "number") {
+    return;
+  }
+
+  if (x < 0) {
     return false;
   }
 
-  if (x === 0) {
-    return true;
-  }
+  const arr = [];
 
-  let number = Math.abs(x);
-
-  const xArr = [];
-
-  while (number) {
-    xArr.unshift(number % 10);
-    number = Math.floor(number / 10);
+  while (x) {
+    arr.unshift(x % 10);
+    x = Math.floor(x / 10);
   }
 
   let left = 0,
-    right = xArr.length - 1;
+    right = arr.length - 1;
 
-  let result = true;
-
-  while (left <= right) {
-    if (xArr[left] !== xArr[right]) {
+  while (left < right) {
+    if (arr[left] !== arr[right]) {
       return false;
     }
-    left++;
-    right--;
+    ++left;
+    --right;
   }
 
-  return result;
+  return true;
 };
 // @lc code=end
 
