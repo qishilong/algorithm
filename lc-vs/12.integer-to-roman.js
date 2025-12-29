8e2f83326dddf0336c72aa6fe33963acfc9681b2
qishilong <663253062@qq.com>
2025-12-29 12:07:10 +0800

    ci(lc-vs): 12. 整数转罗马数字

--- lc-vs/12.integer-to-roman.js
+++ lc-vs/12.integer-to-roman.js
@@ -56,12 +56,76 @@
  * @param {number} num
  * @return {string}
  */
+// var intToRoman = function (num) {
+//   if (!num || typeof num !== "number") {
+//     return;
+//   }
+
+//   // 对每个数字对应的罗马字符硬编码
+//   const thousands = ["", "M", "MM", "MMM"];
+//   const hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+//   const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+//   const ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+
+//   return (
+//     thousands[Math.floor(num / 1000)] +
+//     hundreds[Math.floor((num % 1000) / 100)] +
+//     tens[Math.floor((num % 100) / 10)] +
+//     ones[Math.floor(num % 10)]
+//   );
+// };
+
+/**
+ * 1. 模拟
+ * @param {number} num
+ * @return {string}
+ */
+// var intToRoman = function (num) {
+//   if (!num || typeof num !== "number") {
+//     return;
+//   }
+
+//   const valueSymbol = [
+//     [1000, "M"],
+//     [900, "CM"],
+//     [500, "D"],
+//     [400, "CD"],
+//     [100, "C"],
+//     [90, "XC"],
+//     [50, "L"],
+//     [40, "XL"],
+//     [10, "X"],
+//     [9, "IX"],
+//     [5, "V"],
+//     [4, "IV"],
+//     [1, "I"],
+//   ];
+
+//   const res = [];
+
+//   for (const [val, key] of valueSymbol) {
+//     while (num >= val) {
+//       num -= val;
+//       res.push(key);
+//     }
+//     if (num === 0) {
+//       break;
+//     }
+//   }
+
+//   return res.join("");
+// };
+
+/**
+ * 2. 硬编码
+ * @param {number} num
+ * @return {string}
+ */
 var intToRoman = function (num) {
   if (!num || typeof num !== "number") {
     return;
   }
 
-  // 对每个数字对应的罗马字符硬编码
   const thousands = ["", "M", "MM", "MMM"];
   const hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
   const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
@@ -71,7 +135,7 @@ var intToRoman = function (num) {
     thousands[Math.floor(num / 1000)] +
     hundreds[Math.floor((num % 1000) / 100)] +
     tens[Math.floor((num % 100) / 10)] +
-    ones[Math.floor(num % 10)]
+    ones[num % 10]
   );
 };
 // @lc code=end
